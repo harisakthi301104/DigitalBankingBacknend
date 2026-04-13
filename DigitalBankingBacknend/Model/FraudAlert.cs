9740f0586aff7e35e351e754ac8131b91a4b1ea9
Rubesh <mrubesh270@gmail.com>
2026-04-13 10:51:31 +0530

    Auth-Alert Comitted

---
+++ DigitalBankingBacknend/Model/FraudAlert.cs
@@ -0,0 +1,12 @@
+﻿namespace DigitalBankingBacknend.Model
+{
+    public class FraudAlert
+    {
+        public int Id { get; set; }
+        public int TransactionId { get; set; }
+        public string Reason { get; set; }
+        public int RiskScore { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
+
---
+++ DigitalBankingBacknend/Services/FraudServices.cs
@@ -0,0 +1,71 @@
+﻿using DigitalBankingBacknend.Data;
+using DigitalBankingBacknend.DTO;
+using DigitalBankingBacknend.Model;
+
+namespace DigitalBankingBacknend.Services
+{
+    public class FraudService
+    {
+        private readonly AppDbContext _context;
+
+        public FraudService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public FraudcheckDto CheckFraud(TransferDto dto)
+        {
+            int score = 0;
+            string reason = "";
+
+            // Rule 1: High amount
+            if (dto.Amount > 50000)
+            {
+                score += 50;
+                reason = "High amount";
+            }
+
+            // Rule 2: Too many transactions (last 1 min)
+            var count = _context.Transactions
+                .Count(t => t.FromAccountId == dto.FromAccountId &&
+                            t.Date > DateTime.Now.AddMinutes(-1));
+
+            if (count > 3)
+            {
+                score += 30;
+                reason = "Too many transactions";
+            }
+
+            // Rule 3: Same account transfer
+            if (dto.FromAccountId == dto.ToAccountId)
+            {
+                score += 50;
+                reason = "Invalid self transfer";
+            }
+
+            // Final decision (simple)
+            bool isFraud = score >= 50;
+
+            return new FraudcheckDto
+            {
+                IsFraud = isFraud,
+                RiskScore = score,
+                Reason = reason
+            };
+        }
+
+        public async Task SaveAlert(FraudcheckDto dto)
+        {
+            var alert = new FraudAlert
+            {
+                TransactionId = dto.TransactionId,
+                Reason = dto.Reason,
+                RiskScore = dto.RiskScore,
+                CreatedAt = DateTime.Now
+            };
+
+            _context.FraudAlerts.Add(alert);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
